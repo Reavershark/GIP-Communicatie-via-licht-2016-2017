a33f39930c97e31044c9b4859265c5aad324fb72
Reavershark <jonasty1999@gmail.com>
2017-05-02 20:48:56 +0200

    Add files via upload

---
+++ sketch_mar27c.ino
@@ -0,0 +1,11 @@
+int inputPin = 0; //Index of analog pin used (ex A'0')
+long rate = 9600; //Baud rate
+
+void setup() {  //Hook at boot finish
+  Serial.begin(rate);
+}
+
+void loop() {
+  Serial.println(analogRead(inputPin));
+  delay(200);
+}
