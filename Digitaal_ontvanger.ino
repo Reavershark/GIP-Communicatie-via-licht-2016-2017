2960d184a2382413a9e8ee228c7cf603ee9059ee
Reavershark <jonasty1999@gmail.com>
2017-05-02 20:48:13 +0200

    Add files via upload

---
+++ Digitaal_ontvanger.ino
@@ -0,0 +1,52 @@
+/*  
+  ASCII-decoder
+  Door Jonas Meeuws
+*/
+//Variabelen
+int inputPin = 13; //Index of pin used
+long rate = 9600; //Baud rate
+float del = 500; //Source period
+long trig = 150; //Voltage threshhold
+bool a;
+
+//I/O configureren
+void setup() {  //Hook at boot finish
+  pinMode(inputPin, INPUT);
+  Serial.begin(rate);
+}
+
+//Belangrijkste code
+void loop() { //Repeatedly triggering hook
+    //Variabele
+    char character;
+
+    //4 Controlebits
+    if (analogRead(inputPin) > 150){
+        delay(del);
+        print(analogRead(inputPin))
+        if (analogRead(inputPin) < 150){
+          delay(del);
+          if (analogRead(inputPin) > 150){
+           delay(del);
+            if (analogRead(inputPin) < 150){
+              delay(del);
+
+              //laatste 8 bits opslaan als karakter
+                for (int i=0; i<7; ++i){
+                  if (analogRead(inputPin) < 150){
+                    a = 1;
+                  } else {
+                    a = 0;
+                  }
+                  
+                  bitWrite(character, i, !a);
+                  Serial.print("&");
+                  delay(del);
+                }
+              //Karakter op seriële poort schrijven
+              Serial.print(character, HEX);
+        }
+      }
+    }
+  }
+}
---
+++ Digitaal_zender.ino
@@ -0,0 +1,46 @@
+/*  
+  ASCII-encoder
+  Door Jonas Meeuws
+*/
+//Variabelen
+int ledPin = 13; // The Led's pin number ('Digital' pins on the PBC)
+long rate = 9600;//Delay between transferred bits in ms^-1
+float del = 500; //Source period
+char buff[1];
+
+//I/O configureren
+void setup() {  //Hook at boot finish
+  
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(rate);
+  
+}
+
+//4 controlebits en 1 byte (karakter) verzenden
+void sendChar(char ch) { //Modulates power on ledPin using ASCII encoding
+  
+      digitalWrite(ledPin, HIGH);
+      delay(del);
+      digitalWrite(ledPin, LOW);
+      delay(del);
+      digitalWrite(ledPin, HIGH);
+      delay(del);
+      digitalWrite(ledPin, LOW);
+      delay(del);
+      
+  for(int i=0; i<=7; i = i + 1) { //Run 8 times, for each bit in a byte
+    
+    bool currentBit = bitRead(ch, i); //Store the 'i'th' bit of the char
+    digitalWrite(ledPin, currentBit);
+    delay(del);
+    
+  }
+}
+
+//Seriële poort lezen
+void loop() { //Constantly triggering hook
+  if (Serial.available() > 0) {
+    Serial.readBytes(buff, 1);
+    sendChar(buff[0]);
+  }
+}
